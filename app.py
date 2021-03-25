61457dba1631d75b9ccc0d032c9a7aa3c405d34e
paulinhomacedo <macedopham@gmail.com>
2021-03-25 19:32:29 -0300

    .

--- app.py
+++ app.py
@@ -131,4 +131,5 @@ def _inject_user():
 # allow running from the command line
 if __name__ == '__main__':
     create_tables()
-    app.run()
+    # app.run()
+    app.run(host='0.0.0.0',port = 8000)
